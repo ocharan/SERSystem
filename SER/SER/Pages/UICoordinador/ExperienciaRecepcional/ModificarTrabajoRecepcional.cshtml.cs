6bbeab462178b867a6899a4c5a821b075a54bffc
Josue Capistran García <josuecapistrangarcia@gmail.com>
2022-11-29 05:52:25 -0600

    Modificar trabajo recepcional completado

--- SER/SER/Pages/UICoordinador/ExperienciaRecepcional/ModificarTrabajoRecepcional.cshtml.cs
+++ SER/SER/Pages/UICoordinador/ExperienciaRecepcional/ModificarTrabajoRecepcional.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using NuGet.Protocol;
 using SER.Context;
 using SER.DTO;
 using SER.Entities;
@@ -13,6 +14,10 @@ public class ModificarTrabajoRecepcional : PageModel
     [BindProperty] public TrabajoRecepcional TrabajoRecepcional { get; set; }
     
     [BindProperty] public string tipoTrabajo { get; set; }
+    
+    [BindProperty] public Archivo ArchivoTrabajo { get; set; }
+    
+    [BindProperty] public string estado { get; set; }
     public string proyectoAsociado { get; set; }
     
     public List<Pladeafei> Pladeafeis { get; set; }
@@ -21,19 +26,125 @@ public class ModificarTrabajoRecepcional : PageModel
     
     public List<ProyectoDeInvestigacion> ProyectoDeInvestigacions { get; set; }
 
-    public ModificarTrabajoRecepcional(MySERContext context)
+    private IWebHostEnvironment _environment;
+
+    private bool tieneDocumento = false;
+
+    public ModificarTrabajoRecepcional(MySERContext context, IWebHostEnvironment environment)
     {
+        _environment = environment;
         _context = context;
         TrabajoRecepcional = new TrabajoRecepcional();
         Pladeafeis = new List<Pladeafei>();
         Vinculacions = new List<VinculacionOrg>();
         ProyectoDeInvestigacions = new List<ProyectoDeInvestigacion>();
     }
-    
-    
+
+    public async Task<IActionResult> OnPostModificarTrabajo(IFormFile fileTrabajo)
+    {
+        try
+        {
+            var trabajoModificar = _context.TrabajoRecepcionals.FirstOrDefault(t =>
+                t.TrabajoRecepcionalId.Equals(Int32.Parse(Request.Query["id"])));
+            trabajoModificar.Nombre = Request.Form["NombreTrabajo"];
+            trabajoModificar.Duracion = Request.Form["DuracionTrabajo"];
+            trabajoModificar.Fechadeinicio = DateTime.Parse(Request.Form["FechaTrabajo"]);
+            trabajoModificar.LineaDeInvestigacion = Request.Form["LineaTrabajo"];
+            trabajoModificar.Modalidad = Request.Form["ModalidadTrabajo"];
+            trabajoModificar.Estado = Request.Form["estadoTrabajo"];   
+            var tipoTrabajo = Request.Form["TipoTrabajo"];
+            if (tipoTrabajo == "vinculacion")
+            {
+                trabajoModificar.VinculacionId = Int32.Parse(Request.Form["idProyecto"]);
+            }else if (tipoTrabajo == "pladea")
+            {
+                trabajoModificar.PladeafeiId = Int32.Parse(Request.Form["idProyecto"]);
+            }
+            else
+            {
+                trabajoModificar.ProyectoDeInvestigacionId = Int32.Parse(Request.Form["idProyecto"]);
+            }
+
+            if (fileTrabajo != null)
+            {
+                string fecha = DateTime.Now.ToString().Replace("/", "");
+                string fileName = "Anteproyecto_"+trabajoModificar.TrabajoRecepcionalId+ fecha.Replace(" ", "").Replace(":", "");
+                var archivo = Path.Combine(_environment.WebRootPath, "Archivos", fileName);
+                borrararchivoTrabajo(trabajoModificar.TrabajoRecepcionalId);
+                using (var fileStream = new FileStream(archivo, FileMode.Create))
+                {
+                    await fileTrabajo.CopyToAsync(fileStream);
+                }
+                if (!tieneDocumento)
+                {
+                    var tipo =
+                        _context.TipoDocumentos.FirstOrDefault(t => t.NombreDocumento.Equals("Anteproyecto"));
+                    Documento documento = new Documento();
+                    documento.TrabajoRecepcionalId = trabajoModificar.TrabajoRecepcionalId;
+                    if (tipo == null)
+                    {
+                        TipoDocumento tipoDocumento = new TipoDocumento()
+                        {
+                            NombreDocumento = "Anteproyecto",
+                            ExperienciaEducativa = "Experiencia Recepcional"
+                        };
+                        _context.TipoDocumentos.Add(tipoDocumento);
+                        _context.SaveChanges();
+                        documento.TipoDocumentoId = tipoDocumento.IdTipo;
+                        _context.Documentos.Add(documento);
+                        _context.SaveChanges();
+                    }
+                    else
+                    {
+                        documento.TipoDocumentoId = tipo.IdTipo;
+                        _context.Documentos.Add(documento);
+                        _context.SaveChanges();
+                    }
+                }
+                ArchivoTrabajo = new Archivo();
+                ArchivoTrabajo.NombreArchivo = fileName +"."+fileTrabajo.ContentType.Split("/")[1];
+                ArchivoTrabajo.IdFuente = trabajoModificar.TrabajoRecepcionalId;
+                ArchivoTrabajo.Direccion = "Archivos/" + fileName;
+                ArchivoTrabajo.Fuente = "Anteproyecto";
+                ArchivoTrabajo.TipoContenido = fileTrabajo.ContentType;
+                _context.Archivos.Add(ArchivoTrabajo);
+                _context.SaveChanges();
+                TempData["SuccessMessage"] = "Trabajo recepcional actualizado correctamente";
+            }
+            else
+            {
+                TempData["SuccessMessage"] = "Trabajo recepcional actualizado correctamente";
+                _context.SaveChanges();
+            }
+        }
+        catch (Exception e)
+        {
+            TempData["ErrorMessage"] = "Ha ocurrido un error al actualizar la información, "+e.Message;
+        }
+        cargarInformacionTrabajo();
+        return Page();
+    }
+
+    public void borrararchivoTrabajo(int idTrabajo)
+    {
+        var archivo = _context.Archivos.FirstOrDefault(a => a.IdFuente == idTrabajo && a.Fuente.Equals("Anteproyecto"));
+        if (archivo!=null)
+        {
+            System.IO.File.Delete(_environment.WebRootPath+"/"+archivo.Direccion);
+            _context.Remove(archivo);
+            _context.SaveChanges();
+        }
+    }
     public void OnGet()
     {
-        cargarInformacionTrabajo();
+        try
+        {
+            cargarInformacionTrabajo();
+        }
+        catch (Exception e)
+        {
+            TempData["ErrorMessage"] = "Ha ocurrido un error al cargar la información del trabajo, "+e.Message;
+        }
     }
 
     public void cargarInformacionTrabajo()
@@ -41,6 +152,8 @@ public class ModificarTrabajoRecepcional : PageModel
         var trabajo =
             _context.TrabajoRecepcionals.FirstOrDefault(t =>
                 t.TrabajoRecepcionalId == Int32.Parse(Request.Query["id"]));
+        var archivoTrabajo = _context.Archivos.FirstOrDefault(a => a.IdFuente.Equals(Int32.Parse(Request.Query["id"])) && a.Fuente.Equals("Anteproyecto"));
+        estado = trabajo.Estado;
         TrabajoRecepcional.Nombre = trabajo.Nombre;
         TrabajoRecepcional.Duracion = trabajo.Duracion;
         TrabajoRecepcional.Fechadeinicio = trabajo.Fechadeinicio;
@@ -62,6 +175,14 @@ public class ModificarTrabajoRecepcional : PageModel
             cargarInvestigacion();
             TrabajoRecepcional.ProyectoDeInvestigacionId = trabajo.ProyectoDeInvestigacionId;
         }
+
+        if (archivoTrabajo !=null)
+        {
+            tieneDocumento = true;
+            ArchivoTrabajo = new Archivo();
+            ArchivoTrabajo.NombreArchivo = archivoTrabajo.NombreArchivo;
+            ArchivoTrabajo.Direccion = archivoTrabajo.Direccion;
+        }
     }
 
     public void cargarPladeas()
@@ -72,7 +193,7 @@ public class ModificarTrabajoRecepcional : PageModel
     public void cargarVinculacion()
     {
         var listaVinculacion = _context.Vinculacions.ToList();
-        var listaOrgs = _context.Organizacions.ToList();
+        var listaOrgs = _context.Organizacions.ToList();        
         var vinculaciones = listaVinculacion.Join(listaOrgs, vinculacion => vinculacion.OrganizacionIid,
             organizacion => organizacion.OrganizacionId, (vinculacion, organizacion) => new
             {
@@ -93,5 +214,56 @@ public class ModificarTrabajoRecepcional : PageModel
     public void cargarInvestigacion()
     {
         ProyectoDeInvestigacions = _context.ProyectoDeInvestigacions.ToList();
+    }
+    
+    public JsonResult OnGetObtenerPladeaModificar()
+    {
+        var listaPladea = _context.Pladeafeis.ToList();
+        foreach (var pladea in listaPladea)
+        {
+            Pladeafei pladeafei = new Pladeafei()
+            {
+                Accion = pladea.Accion,
+                PladeafeiId = pladea.PladeafeiId
+            };
+            Pladeafeis.Add(pladeafei);
+        }
+
+        return new JsonResult(Pladeafeis.ToJson());
+    }
+
+    public JsonResult OnGetObtenerInvestigacionModificar()
+    {
+        var listaInvestigacion = _context.ProyectoDeInvestigacions.ToList();
+        foreach (var investigacion in listaInvestigacion)
+        {
+            ProyectoDeInvestigacion proyectoDeInvestigacion = new ProyectoDeInvestigacion()
+            {
+                Nombre = investigacion.Nombre,
+                ProyectoDeInvestigacionId = investigacion.ProyectoDeInvestigacionId
+            };
+            ProyectoDeInvestigacions.Add(proyectoDeInvestigacion);
+        }
+        return new JsonResult(ProyectoDeInvestigacions.ToJson());
+    }
+
+    public JsonResult OnGetObtenerVinculacionModificar()
+    {
+        var listaVinculacion = _context.Vinculacions.ToList();
+        var listaOrgs = _context.Organizacions.ToList();
+        var vinculaciones = listaVinculacion.Join(listaOrgs, vinculacion => vinculacion.OrganizacionIid,
+            organizacion => organizacion.OrganizacionId, (vinculacion, organizacion) => new
+            {
+                nombre = organizacion.Nombre,
+                id = vinculacion.VinculacionId
+            });
+        return new JsonResult(vinculaciones.ToJson());
+    }
+    
+    public IActionResult OnPostFile()
+    {
+        var id = Request.Query["id"];
+        var fileTrabajo = _context.Archivos.FirstOrDefault(a => a.IdFuente == Int32.Parse(id));
+        return File(fileTrabajo.Direccion, fileTrabajo.TipoContenido);
     }
 }
--- SER/SER/Pages/UICoordinador/ExperienciaRecepcional/NuevoTrabajoRecepcional.cshtml.cs
+++ SER/SER/Pages/UICoordinador/ExperienciaRecepcional/NuevoTrabajoRecepcional.cshtml.cs
@@ -21,20 +21,28 @@ public class NuevoTrabajoRecepcional : PageModel
     public List<Vinculacion> Vinculacions { get; set; }
     
     public List<ProyectoDeInvestigacion> ProyectoDeInvestigacions { get; set; }
+
+    private IWebHostEnvironment _environment;
     
-    public NuevoTrabajoRecepcional(MySERContext context)
+    public Archivo archivoTrabajo { get; set; }
+    
+    public Documento Documento { get; set; }
+    
+    public NuevoTrabajoRecepcional(MySERContext context, IWebHostEnvironment environment)
     {
         _context = context;
         Pladeafeis = new List<Pladeafei>();
         Vinculacions = new List<Vinculacion>();
         ProyectoDeInvestigacions = new List<ProyectoDeInvestigacion>();
+        _environment = environment;
+        archivoTrabajo = new Archivo();
     }
     
     public void OnGet()
     {
     }
 
-    public void OnPost()
+    public async Task<IActionResult> OnPost(IFormFile fileTrabajoRecepcional)
     {
         if (proyectoAsociado == "vinculacion")
         {
@@ -49,12 +57,53 @@ public class NuevoTrabajoRecepcional : PageModel
         }
         try
         {
-            TrabajoRecepcional.Estado = "Activo";
+            TrabajoRecepcional.Estado = "En proceso";
             bool existe = _context.TrabajoRecepcionals.Any(t => t.Nombre == TrabajoRecepcional.Nombre);
             if (!existe)
             {
                 _context.TrabajoRecepcionals.Add(TrabajoRecepcional);
                 _context.SaveChanges();
+                if (fileTrabajoRecepcional != null)
+                {
+                    string fecha = DateTime.Now.ToString().Replace("/", "");
+                    string fileName = "Anteproyecto_"+TrabajoRecepcional.TrabajoRecepcionalId+ fecha.Replace(" ", "").Replace(":", "");
+                    var archivo = Path.Combine(_environment.WebRootPath, "Archivos", fileName);
+                    using (var fileStream = new FileStream(archivo, FileMode.Create))
+                    {
+                        await fileTrabajoRecepcional.CopyToAsync(fileStream);
+                    }
+
+                    Documento = new Documento();
+                    Documento.TrabajoRecepcionalId = TrabajoRecepcional.TrabajoRecepcionalId;
+                    var Tipo = _context.TipoDocumentos.FirstOrDefault(t => t.NombreDocumento.Equals("Anteproyecto"));
+                    if (Tipo != null)
+                    {
+                        Documento.TipoDocumentoId = Tipo.IdTipo;
+                        _context.Documentos.Add(Documento);
+                        _context.SaveChanges();
+                    }
+                    else
+                    {
+                        TipoDocumento tipoDocumento = new TipoDocumento()
+                        {
+                            NombreDocumento = "Anteproyecto",
+                            ExperienciaEducativa = "Experiencia Recepcional"
+                        };
+                        _context.TipoDocumentos.Add(tipoDocumento);
+                        _context.SaveChanges();
+                        Documento.TipoDocumentoId = tipoDocumento.IdTipo;
+                        _context.Add(Documento);
+                        _context.SaveChanges();
+                    }
+                    archivoTrabajo.NombreArchivo = fileName +"."+fileTrabajoRecepcional.ContentType.Split("/")[1];
+                    archivoTrabajo.IdFuente = TrabajoRecepcional.TrabajoRecepcionalId;
+                    archivoTrabajo.Direccion = "Archivos/" + fileName;
+                    archivoTrabajo.Fuente = "Anteproyecto";
+                    archivoTrabajo.TipoContenido = fileTrabajoRecepcional.ContentType;
+                    _context.Archivos.Add(archivoTrabajo);
+                    _context.SaveChanges();
+                    TempData["Success"] = "Trabajo recepcional registrado correctamente";
+                }
                 TempData["Success"] = "Trabajo recepcional registrado correctamente";
             }
             else
@@ -66,6 +115,7 @@ public class NuevoTrabajoRecepcional : PageModel
         {
             TempData["Error"] = "Ha ocurrido un error durante el registro " + e.Message;
         }
+        return Page();
     }
 
     public JsonResult OnGetObtenerPladea()
